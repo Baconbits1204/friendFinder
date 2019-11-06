1b7b04cb6bf34a2c78cb76da3cd7f1f517036187
baconbits1204 <rafaelhermoso1204@gmail.com>
2019-11-05 21:05:04 -0500

    changing + to , in html routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,13 +7,13 @@ module.exports = function(app){
   //default GET route that leads to survey
   app.get('/survey', function (req, res) {
      console.log(req);
-     res.sendFile(path.join(__dirname + '../public/survey.html'))
+     res.sendFile(path.join(__dirname, '../public/survey.html'))
   });
 
   //a USE route to home page
   app.use(function (req, res) {
     console.log(req);
-    res.sendFile(path.join(__dirname + '/../public/home.html'));
+    res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 };
 
